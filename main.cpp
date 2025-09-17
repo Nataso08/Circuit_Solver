020cabc1e423d1d80f8fb35550d186ccdea37a47
Nataso08 <elia.maso18@gmail.com>
2025-09-17 21:01:28 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -15,30 +15,68 @@ inline float parallel (float a, float b) {
 }
 
 
+void solve_section (vector<string>& tokens, vector<string>::iterator start) {
+    int n = find (start, tokens.end(), ")") - start;
 
-void find_brackets (vector<string>& tokens) {
-    vector<string>::iterator open_bracket, 
-        close_bracket = find(tokens.begin(), tokens.end(), ")");
+    while (n > 1) {
+        vector<string>::iterator O = find (start, start + n, "//");
 
-    if (close_bracket != tokens.end()) {
-        for (open_bracket=close_bracket; open_bracket >= tokens.begin(); open_bracket--) {
-            if (*open_bracket == "(") {
-                break;
-            }
+        if (O != start + n) {
+            *O = to_string(parallel (stoi(*(O - 1)), stoi(*(O + 1))));    
+        } else {
+            O = find (start, start + n, "+");
+
+            *O = to_string(serie (stoi(*(O - 1)), stoi(*(O + 1))));
         }
+
+        tokens.erase(O +1);
+        tokens.erase(O -1);
+        
+        n -= 2;
     }
 }
 
-void solve_section () {
+// individuo le parentesi più interne
+pair<vector<string>::iterator, vector<string>::iterator> find_brackets (vector<string>& tokens) {
+    pair<vector<string>::iterator, vector<string>::iterator> brackets;
+    
+    brackets.second = find(tokens.begin(), tokens.end(), ")");
 
+    if (brackets.second != tokens.end()) {
+        for (brackets.first=brackets.second; brackets.first >= tokens.begin(); brackets.first--) {
+            if (*brackets.first == "(") {
+                break;
+            }
+        }
+    } else {
+        brackets.first = tokens.end();
+    }
+
+    return brackets;
+}
+
+// risolvo brackets
+void solve_brackets (vector<string>& tokens, vector<string>::iterator brackets) {
+    solve_section (tokens, brackets + 1);
+
+    tokens.erase(brackets + 2);
+    tokens.erase(brackets);
 }
 
 
-void solve () {
+void solve (vector<string>& tokens) {
+    vector<string>::iterator brackets;
 
+    while (tokens.size() > 1) {
+        brackets = find_brackets (tokens).first;
+
+        if (brackets != tokens.end()) {   // esistono parentesi
+            solve_brackets (tokens, brackets);
+        } else {
+            solve_section (tokens, tokens.end());
+        }
+    }
 }
-
-
 
 int main () {
     string input;
@@ -54,9 +92,7 @@ int main () {
         while (ss >> token) tokens.push_back(token);
     }
 
-    while (tokens.size() > 1) {
-
-    }
+    solve (tokens);
 
     cout << "R_eq = " << tokens[0] << " ohm";
 
